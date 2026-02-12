52dcb5784ba2a249d03ce558825d51d0693b9ba5
davidyeat <david.yeat123@gmail.com>
2026-02-12 16:24:56 +0700

    frontend_week05

---
+++ start_code_4/exercise-2/exercise2/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./components/Header.jsx";
+import Main  from "./components/Main.jsx";
+import Footer from "./components/Footer.jsx";
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+      {/* <h1>Hello</h1> */}
+    </>
+  )
+}
+
+export default App;
