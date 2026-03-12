46587189cf7b5138d72abdc86e9ae448156ed3d4
davidyeat <david.yeat123@gmail.com>
2026-03-12 16:40:49 +0700

    ReactJS-Lab-week08: Going deeper with states

--- start_code_8/exercise-3/src/App.jsx
+++ start_code_8/exercise-3/src/App.jsx
@@ -23,6 +23,30 @@ const ORDERS = [
 
 export default function App() {
   const [orders, setOrders] = React.useState(ORDERS);
+
+  function calculateTotal() {
+    let total = 0;
+    total = orders.reduce((acc, order) => acc + (order.price * order.quantity), 0);
+    return total;
+  }
+
+  function handleIncrease(product) {
+    setOrders(orders.map((order) => {
+      if (order.product === product) {
+        return {...order, quantity: order.quantity + 1};
+      }
+      return order;
+    }))
+  }
+
+  function handleDecrease(product) {  
+    setOrders(orders.map((order) => {
+      if (order.product === product) {
+        return {...order, quantity: order.quantity - 1 > 0 ? order.quantity - 1 : 0};
+      } 
+      return order;
+    }))
+  }
   
 
   return (
@@ -33,11 +57,12 @@ export default function App() {
 
       <div className="order-list">
         {orders.map((order, index) => (
-          <OrderCard key={index} product={order.product} price={order.price} quantity={order.quantity}></OrderCard>
+          <OrderCard key={index} 
+                    order={order} onIncrease={handleIncrease} onDecrease={handleDecrease}></OrderCard>
         ))}
       </div>
 
-      <CheckoutButton total="TODO"></CheckoutButton>
+      <CheckoutButton total={calculateTotal()}></CheckoutButton>
     </>
   );
 }
--- start_code_8/exercise-3/src/components/OrderCard.jsx
+++ start_code_8/exercise-3/src/components/OrderCard.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 
-export default function OrderCard({product, price, quantity}) {
-  const format = (price) => "$ " + price;
+export default function OrderCard({order, onIncrease, onDecrease}) {
 
+  const format = (price) => "$ " + price;
 
   return (
     <div className="order">
       <div>
-        <h4>{product}</h4>
-        <small>{format(price)}</small>
+        <h4>{order.product}</h4>
+        <small>{format(order.price)}</small>
       </div>
 
       <div className="order-quantity">
-        <div className="order-button">-</div>
-        <h4>{quantity}</h4>
-        <div className="order-button">+</div>
+        <div className="order-button" onClick={() => onDecrease(order.product)}>-</div>
+        <h4>{order.quantity}</h4>
+        <div className="order-button" onClick={() => onIncrease(order.product)}>+</div>
       </div>
     </div>
   );
